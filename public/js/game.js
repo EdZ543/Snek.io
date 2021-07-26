e3cc6432844233e3067e2e14df2ec60442f47a74
Eddie Zhuang <zhuang.eddie@gmail.com>
2021-07-26 15:38:20 -0400

    Slithering Perfected

--- public/js/game.js
+++ public/js/game.js
@@ -23,14 +23,12 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-var SPEED = 100;
-var ROTATION_SPEED = 1 * Math.PI;
+var SPACING = 30;
+var ROTATION_SPEED = 1.5 * Math.PI;
 var ROTATION_SPEED_DEGREES = Phaser.Math.RadToDeg(ROTATION_SPEED);
 var TOLERANCE = 0.05 * ROTATION_SPEED;
 var PLAYER_SIZE = 20;
-var SECTION_SPACING = 10;
-
-var velocityFromRotation = Phaser.Physics.Arcade.ArcadePhysics.prototype.velocityFromRotation;
+var SPEED = 5;
 
 function preload() {
   this.load.image('background', 'assets/background.jpg')
@@ -41,7 +39,7 @@ function create() {
   var self = this;
   this.socket = io();
   this.otherPlayers = this.physics.add.group();
-  this.section = [];
+  this.nodes = [];
   this.path = [];
   
   this.socket.on('currentPlayers', (players) => {
@@ -69,11 +67,7 @@ function create() {
   this.socket.on('playerMoved', function (playerInfo) {
     self.otherPlayers.getChildren().forEach((otherPlayer) => {
       if (playerInfo.playerId === otherPlayer.playerId) {
-        if (otherPlayer.sectionId == -1) {
-          otherPlayer.setPosition(playerInfo.head.x, playerInfo.head.y);
-        } else {
-          otherPlayer.setPosition(playerInfo.section[otherPlayer.sectionId].x, playerInfo.section[otherPlayer.sectionId].y);
-        }
+          otherPlayer.setPosition(playerInfo.nodes[otherPlayer.nodeId].x, playerInfo.nodes[otherPlayer.nodeId].y);
       }
     });
   });
@@ -81,46 +75,51 @@ function create() {
 }
 
 function update() {
-  if (this.head) {
-    this.input.activePointer.updateWorldPoint(this.cameras.main);
-    var angleToPointer = Phaser.Math.Angle.Between(this.head.x, this.head.y, this.input.activePointer.worldX, this.input.activePointer.worldY);
-    var angleDelta = Phaser.Math.Angle.Wrap(angleToPointer - this.head.rotation);
-    
-    if (Phaser.Math.Within(angleDelta, 0, TOLERANCE)) {
-      this.head.rotation = angleToPointer;
-      this.head.body.setAngularVelocity(0);
-    } else {
-      this.head.body.setAngularVelocity(Math.sign(angleDelta) * ROTATION_SPEED_DEGREES);
-    }
-    
-    velocityFromRotation(this.head.rotation, SPEED, this.head.body.velocity);
-    
-    this.socket.emit('playerMovement', { x: this.head.x, y: this.head.y, section: this.section });
-    
-    var part = this.path.pop();
-    part.x = this.head.x
-    part.y = this.head.y;
-    this.path.unshift(part);
-    for (var i = 0; i < this.section.length; i++) {
-      this.section[i].x = this.path[i * SECTION_SPACING].x;
-      this.section[i].y = this.path[i * SECTION_SPACING].y;
-    }
+  if (this.nodes.length > 0) {
+    for (var i = 0; i < this.nodes.length; i++) {
+      if (i == 0) {
+        this.input.activePointer.updateWorldPoint(this.cameras.main);
+        var angleToPointer = Phaser.Math.Angle.Between(this.nodes[i].x, this.nodes[i].y, this.input.activePointer.worldX, this.input.activePointer.worldY);
+        var angleDelta = Phaser.Math.Angle.Wrap(angleToPointer - this.nodes[i].rotation);
+        
+        if (Phaser.Math.Within(angleDelta, 0, TOLERANCE)) {
+          this.nodes[i].rotation = angleToPointer;
+          this.nodes[i].body.setAngularVelocity(0);
+        } else {
+          this.nodes[i].body.setAngularVelocity(Math.sign(angleDelta) * ROTATION_SPEED_DEGREES);
+        }
 
-    this.physics.add.collider(this.head, this.otherPlayers, (head, otherPlayer) => {
-      this.socket.emit('gameOver');
-    });
+        var xDir = Math.cos(this.nodes[i].rotation);
+        var yDir = Math.sin(this.nodes[i].rotation);
+        this.nodes[i].x += xDir * SPEED;
+        this.nodes[i].y += yDir * SPEED;
+
+        for (var j = 0; j < SPEED; j++) {
+          var part = this.path.pop();
+          part.x = this.nodes[i].x + xDir;
+          part.y = this.nodes[i].y + yDir;
+          this.path.unshift(part);
+        }
+      } else {
+        this.nodes[i].x = this.path[i * SPACING].x;
+        this.nodes[i].y = this.path[i * SPACING].y;
+      }
+    }
+    
+    this.socket.emit('playerMovement', { nodes: this.nodes });
   }
 }
 
 function addPlayer(self, playerInfo) {
-  self.cameras.main.startFollow(self.head);
-  
-  for (var i = 0; i < playerInfo.section.length; i++) {
-    self.section[i] = self.add.circle(playerInfo.section[i].x, playerInfo.section[i].y, PLAYER_SIZE, Phaser.Display.Color.HexStringToColor(playerInfo.color).color);
+  for (var i = 0; i < playerInfo.nodes.length; i++) {
+    self.nodes[i] = self.add.circle(playerInfo.nodes[i].x, playerInfo.nodes[i].y, PLAYER_SIZE, Phaser.Display.Color.HexStringToColor(playerInfo.color).color);
+    self.physics.add.existing(self.nodes[i]);
   };
+
+  self.cameras.main.startFollow(self.nodes[0]);
   
-  for (var i = 0; i <= playerInfo.section.length * SECTION_SPACING; i++) {
-    self.path[i] = {x: playerInfo.head.x, y: playerInfo.head.y};
+  for (var i = 0; i <= playerInfo.nodes.length * SPACING; i++) {
+    self.path[i] = { x: playerInfo.nodes[0].x - i, y: playerInfo.nodes[0].y };
   }
 }
 
@@ -131,7 +130,7 @@ function addOtherPlayers(self, playerInfo) {
   otherHead.sectionId = -1;
   self.otherPlayers.add(otherHead);
   
-  for (var i = 0; i < playerInfo.section.length; i++) {
+  for (var i = 0; i < playerInfo.nodes.length; i++) {
     var otherSection = self.add.circle(playerInfo.section[i].x, playerInfo.section[i].y, PLAYER_SIZE, Phaser.Display.Color.HexStringToColor(playerInfo.color).color);
     self.physics.add.existing(otherSection);
     otherSection.playerId = playerInfo.playerId;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
+const { SocketAddress } = require('net');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var players = {};
 
-var INIT_SECTIONS = 6;
-var SPEED = 50;
-
+var INIT_NODES = 6;
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -17,21 +16,17 @@ io.on('connection', function (socket) {
   console.log('a user connected');
 
   players[socket.id] = {
-    head: {
-      x: Math.floor(Math.random() * 700) + 50,
-      y: Math.floor(Math.random() * 500) + 50,
-    },
-    section: [],
+    nodes: [],
     playerId: socket.id,
     color: Math.floor(Math.random() * 16777215).toString(16),
     score: 0
   }
 
-  for (var i = 0; i < INIT_SECTIONS; i++) {
+  for (var i = 0; i < INIT_NODES; i++) {
     if (i == 0) {
       players[socket.id].nodes[i] = { x: Math.floor(Math.random() * 700) + 50, y: Math.floor(Math.random() * 500) + 50 };
     } else {
-      players[socket.id].nodes[i] = { x: players[socket.id].nodes[i - 1].x - SPEED, y: players[socket.id].nodes[i - 1].y };
+      players[socket.id].nodes[i] = { x: players[socket.id].nodes[i - 1].x, y: players[socket.id].nodes[i - 1].y };
     }
   }
 
@@ -45,13 +40,10 @@ io.on('connection', function (socket) {
     io.emit('unconnect', socket.id);
   });
 
-  socket.on('gameOver', () => {
-    delete players[socket.id];
-    io.emit('unconnect', socket.id);
-  });
-
   socket.on('playerMovement', function (movementData) {
-    players[socket.id].nodes = movementData.nodes;
+    for (var i = 0; i < players[socket.id].nodes.length; i++) {
+      players[socket.id].nodes[i] = movementData.nodes[i];
+    }
     socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 });
